29f1059f10fea19410f39ee2d9b46f50f461b9ca
qhj7 <qhj7@txstate.edu>
2026-09-15 02:09:47 -0500

    Completed Task 3 Variables

--- javascript/external.js
+++ javascript/external.js
@@ -1,5 +1,8 @@
 //document.write("Where is my JS code?");
 //window.alert(5+6);
 //window.alert('5+6')
-document.write('<h1>A heading</h1>');
-document.write('<p>A sentence.</p>');
+//document.write('<h1>A heading</h1>');
+//document.write('<p>A sentence.</p>');
+//A prompt box is used to prompt users to input a vale before entering a page.
+user_name = window.prompt("Please ener your name","Type your name here");
+document.write(user_name);
