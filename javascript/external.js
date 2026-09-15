58f21e90c11a68a7623a65f78af0e1c1fb0aaedf
qhj7 <qhj7@txstate.edu>
2026-09-15 02:47:59 -0500

    Completed Task 5 Conditional Statement

--- javascript/external.js
+++ javascript/external.js
@@ -1,15 +1,14 @@
-//document.write("Where is my JS code?");
-//window.alert(5+6);
-//window.alert('5+6')
-//document.write('<h1>A heading</h1>');
-//document.write('<p>A sentence.</p>');
-//A prompt box is used to prompt users to input a vale before entering a page.
-// user_name = window.prompt("Please ener your name","Type your name here");
-// document.write(user_name);
-a = 22;
-const b = 33;
-document.writeln(a + b);
-document.writeln("<br>");
-document.writeln(a += b);
-document.writeln("<br>");
-document.writeln(a += b);
+document.writeln("<botton onclick='condition()'>Conditional Test</button>");
+
+function condition()
+{
+  x = confirm("Are you sure you want to proceed?");
+  if(x)
+  {
+    document.writeln("You chose Okay!");
+  }
+  else
+   {
+     document.writeln("You chose Cancel!");
+  }
+}
