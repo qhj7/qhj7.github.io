291d37869a60890538a090a5feef20a912967b84
qhj7 <qhj7@txstate.edu>
2026-09-15 02:20:48 -0500

    Completed Task 4 Operators

--- javascript/external.js
+++ javascript/external.js
@@ -4,5 +4,12 @@
 //document.write('<h1>A heading</h1>');
 //document.write('<p>A sentence.</p>');
 //A prompt box is used to prompt users to input a vale before entering a page.
-user_name = window.prompt("Please ener your name","Type your name here");
-document.write(user_name);
+// user_name = window.prompt("Please ener your name","Type your name here");
+// document.write(user_name);
+a = 22;
+const b = 33;
+document.writeln(a + b);
+document.writeln("<br>");
+document.writeln(a += b);
+document.writeln("<br>");
+document.writeln(a += b);
